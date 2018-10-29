a998bf86b54e94f7ba6772d8c7b696c4c6a266c6
Herman <hrivas5726@aol.com>
2018-10-29 19:35:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file.
+function lowerCaseDrivers (drivers) {
+  //return lowercase drivers
+  return drivers.map(lowerCaseName)
+}
+  
+ function lowerCaseName(name) {
+    return name.toLowerCase()
+}
+
+function nameToAttributes(drivers){
+//return array of object, each object is a driver with a first and last name
+ return drivers.map(giveAndTake)
+}
+
+function giveAndTake(fullname) {
+  const splitNames = fullname.split(' ');
+  const driversName = {firstName: splitNames[0], lastName: splitNames[1]};
+  return driversName
+}
+
+//still working on this function(stuck lol)
+function attributesToPhrase (drivers) {
+  return
+}
+
+
